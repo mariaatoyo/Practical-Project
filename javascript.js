06efae9543669eea67ff59f7c578f71c74b32147
Maria Atoyo <P15179843@LEC-ADMIN.dmu.ac.uk>
2017-12-18 23:14:40 +0000

    search bar added

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ function openNav() {
 	document.getElementById("mySidenav").style.display = "block";
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+	//document.getElementById("main").onclick="openNav()" &#9776; MENU;
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
