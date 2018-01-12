55ec98571343653b52774c4598edf90886edcf1a
Maria Atoyo <P15179843@LEC-ADMIN.dmu.ac.uk>
2018-01-12 23:57:05 +0000

    validation

--- javascript.js
+++ javascript.js
@@ -1,65 +1,87 @@
 //SIDE BAR MENU
 function openNav() {
-	document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("mySidenav").style.display = "block";
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-	//document.getElementById("main").onclick="openNav()" &#9776; MENU;
+    //document.getElementById("main").onclick="openNav()" &#9776; MENU;
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
 
-	document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("mySidenav").style.display = "none";
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
+}
+
+function searchBtn() {
+    var txt;
+    if (confirm("Press a button!") == true) {
+        txt = "You pressed OK!";
+    document.getElementById("demo").innerHTML = txt;
 }
 
 // GOOGLE MAPS
 
-function initMap(){
-	//Map options
-	var options = {
-		zoom:3,
-		 center:{lat:-8.7832,lng:-55.4915}
-	}
-	//New map
-	var map = new
-	google.maps.Map(document.getElementById('map'),options);
+function initMap() {
+    //Map options
+    var options = {
+        zoom: 3,
+        center: {
+            lat: -8.7832,
+            lng: -55.4915
+        }
+    }
+    //New map
+    var map = new
+    google.maps.Map(document.getElementById('map'), options);
 
-	//MARKERS
-	var marker = new google.maps.Marker({
-	  position:{lat:13.7942,lng:-88.8965},
-			map:map,
-	  });
-	  		google.maps.event.addListener(marker, 'click', function () {
-  window.location.href="el-salvador.html";
-	  });
-	  
-	var marker = new google.maps.Marker({
-	  position:{lat:23.6345,lng:-102.5528},
-				map:map
-	  });
-	  google.maps.event.addListener(marker, 'click', function () {
-  window.location.href="mexico.html";
-	  });
-	  
-	var marker = new google.maps.Marker({
-	  position:{lat:9.7489,lng:-83.7534},
-				map:map
-	  });
-	  google.maps.event.addListener(marker, 'click', function () {
-  window.location.href="costa-rica.html";
-	  });
-	  
-	var marker = new google.maps.Marker({
-	  position:{lat:9.1900,lng:-75.0152},
-				map:map
-	  }); 
-	  google.maps.event.addListener(marker, 'click', function () {
-  window.location.href="machu-pichu.html";
-	  });
-  
+    //MARKERS
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 13.7942,
+            lng: -88.8965
+        },
+        map: map,
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        window.location.href = "el-salvador.html";
+    });
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 23.6345,
+            lng: -102.5528
+        },
+        map: map
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        window.location.href = "mexico.html";
+    });
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 9.7489,
+            lng: -83.7534
+        },
+        map: map
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        window.location.href = "costa-rica.html";
+    });
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 9.1900,
+            lng: -75.0152
+        },
+        map: map
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        window.location.href = "machu-pichu.html";
+    });
+
 
 }
 
@@ -72,15 +94,17 @@ function showSlides() {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
+        slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
@@ -99,27 +123,31 @@ var slideIndex = 1;
 displaySlides(slideIndex);
 
 function plusSlides(n) {
-  displaySlides(slideIndex += n);
+    displaySlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  displaySlides(slideIndex = n);
+    displaySlides(slideIndex = n);
 }
 
 function displaySlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("desktop-slides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+    var i;
+    var slides = document.getElementsByClassName("desktop-slides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
